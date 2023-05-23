22a194bc5603e8a119f6f3a303ecf903b56c8399
= <=>
2023-05-23 10:00:47 -0300

    api

---
+++ DeepSoundiOS/AppSettings/AppSettings.swift
@@ -0,0 +1,85 @@
+//
+//  AppSettings.swift
+//  DeepSoundiOS
+//
+//  Created by Macbook Pro on 26/06/2019.
+//  Copyright © 2019 Muhammad Haris Butt. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import DeepSoundSDK
+
+struct AppConstant {
+//
+//deepsound key
+    static let key = "Vm0weE5GVXhVWGxWV0doWVYwZDRWMWxVUm5kVlJsbDNWbTFHYVUxV1dubFdiVEZIVmpBeFYyTkVRbUZXVm5Cb1ZrZDRZV015VGtaaFJtaFlVMFZLU1ZadGRGWmxSbVJIVm01S1dHSkhVbFJhVjNoaFpWWmFkRTFJYUZSTlZUVkpWbGR3WVZaSFNraGhSemxWVm5wR2RsbFZXbUZXYkdSeVYyeENWMkV3Y0ZSV1ZWcFNaREpTYzFGc1VtdFNiWGhVVlRCYVMxUXhVbGxqUnpWUVZsaFJNVlF4V210VWJGcDFVV3hzV0ZZelFraFdiVEZTWkRBeFYxZHRhRk5pUm5CMlZrWmpNV0l4V1hoWGJsSnJVakJhY2xSV1drdGxiR3QzVjIxMFYySlZjSGxVYkdoSFYyMUZlVlZzVW1GV00yaHlWbXhhVTJSRk9WaGhSVFZvVFZacmVWWXhXbGRWTVVWNFYxaG9XR0ZzY0ZSV2ExWkxWMVphZEdWSVpHcGlSbG93V2xWVk5XRnNXblJWYm5CYVRVZFNkbGxWWkZkak1XUnpZMFprVG1Kc1NtOVdNVnByVlcxV1IxVnVUbUZTYkhCd1dXdFdkMlZHV2xoalJUbFNUV3MxU0ZZeU5VZFdWMHBWVm01T1dtRXlVbFJhVmxwaFpFZE9ObEpzVm1sU2JYZDZWbXBLZDFFeFduSk5WbWhvVTBoQ1dGVnFUbTlVUmxaelYydGtWMVl3V1RKVVZXaExWRmRXY1ZKc1RtRlNNMEo2VmtSR1UxZEdiM3BUYXpGc1lXdGFWMWRXVWtkVE1WSkhZa1pvYW1WclNuTldiWGhMVm14VmVXTjZWbWxTTUZZMVdrVlNZVll4U25OalNFcFdUV3BHV0ZreWN6RlhWa3B6V2tkc1dGSnJjRkZXTVdRMFZqRlpkMDVZVGxaaVIxSlhXV3hvUTFReFVsWlZiR1JUWWtad1dGWlhjelZXVlRGV1lrUlNXazFIYUVSV01HUkxWbFpLY21GR2NGZFdia0kyVjJ0V1lWTXhXbFpOVlZaT1UwVndUbHBYY0VabGJHUnlWbXhhVGsxV1drcFdSbWhMVkZkV2NWSnNhRnBpUjJoVVdWVmFZV05XVG5KWGJYaFRZWHBWZUZaVVNucE9WbGw1VTJ0c1VtSkZjRmhaYkdodlkyeHNjVkp0UmxkTlYxSXhWVEo0YjJGRk1IaFRiV2hYWWxSQ05GUnJaRmRqTVdSMVZXczVWMVpIZUZkWFYzUlhaREpXYzFaWWJHeFNNMUpZVkZaYWMwNXNXbGhrUkZKWVlrWndSMVZ0Y0ZOV2JVcEhZMFY0V21WcldtaFpla1poWXpGd1IxcEhhRTVOUlhCSVZtMHdlR1F4VFhkT1ZXUnBVbTFTVlZZd1pHOVhWbXgwWkVkR1dGWnRlSGxXTWpGSFZqRktkR1ZJY0ZkTmJsRjNWbXRhWVdSR1ZuTmhSbVJwVjBkb2IxZFhkR0ZUTWxKSFZtNUthRkp0VWs5WmJURnZUbXhhZEUxSWFGUk5WWEI2VmtjMVQyRkdTbk5YYkdoYVlrZFNkbFZzV210ak1WcDBVbXh3VjJGNlZrcFdhMlF3V1ZkRmVGTnVUbXBTVkd4aFZtdFdZVTB4VlhkWGJVWlRUVmRTZWxkclpITlViRmw0VTJ0c1YyRnJiM2RYVm1SSFl6Rk9kVk5zYUdsU01VcG9WbTF3VDFVeFVrZGlSbHBZWW10d2NsUldWbmRXYkd4eVYydGtWMkpHY0VaVlZsSldVRkU5UFE9PQ==/LHBhc3M9/ZFV4YU0wNHRhajlIWVNRcVJHVmxjRk52ZFc1a0tpUkdNMU00ZVV4ZVJTWnljakk1V1MwNGNFRis="
+    
+}
+
+struct ControlSettings {
+    
+    static let showSocicalLogin = false
+    static let hideFacebookLogin = false
+    static let hideGoogleLogin = false
+    static let hideWoWonderLogin = false
+    static let showHideDownloadBtn = false
+    static let  appName = "DeepSound"
+    static let googleClientKey = "497109148599-u0g40f3e5uh53286hdrpsj10v505tral.apps.googleusercontent.com"
+    static let wowonder_ServerKey = "131c471c8b4edf662dd0ebf7adf3c3d7365838b9"
+    static let wowonder_URL = "https://demo.wowonder.com/"
+    static let oneSignalAppId = "69bc0a81-71c3-409f-921d-01c01a44c278"
+    static let isPurchase = true
+    static let addUnitId = "ca-app-pub-3940256099942544/2934735716"
+    static let  interestialAddUnitId = "ca-app-pub-3940256099942544/4411468910"
+    static let paypalAuthorizationToken = "sandbox_zjzj7brd_fzpwr2q9pk2m568s"
+    static let BrainTreeURLScheme = "com.DeepSound.ScripSun.App.payments"
+    static let googleApiKey = "AIzaSyB9hBrJwdQM3oycf_j-53XKrG-hxW9rYt0"
+    static let  HelpLink = "\(API.baseURL)/contact"
+    static let  reportlink = "\(API.baseURL)/contact"
+    static let  termsOfUse = "\(API.baseURL)/terms/terms"
+    static let  privacyPolicy = "\(API.baseURL)/terms/privacy"
+    static let  inviteFriendText = "Please vist our website \(API.baseURL)"
+    //Only for extended customer you can't easily open it
+    static let  applyPayMerchchat = "merchant.com.DeepSound.ScripSun.App"
+  //Payments
+    static let  razorPayInitialkey = "rzp_test_dJRabCgVoFHAFh"
+    static let  securionPayPublicKey = "pk_test_WoOlrf6NeiNsQkq9UBDz9Fsn"
+    static let  securionBundleID = "com.securionpay.sdk.SecurionPay.Examples"
+    
+    static var shouldShowAddMobBanner:Bool = true
+    static var interestialCount:Int? = 3
+    static var isApplePay:Bool? = true
+    static var upgradePaymentAmount:Double? = 9.0
+    static var isGoProEnabled:Bool = true
+    static var IsPaypalEnabled:Bool = true
+    static var isBankTransferEnabled:Bool = true
+    static var IsCreditEnabled:Bool = true
+    static var IsRazorPayEnabled:Bool = true
+    static var IsPayStackEnabled:Bool = true
+    static var IsCashFreeEnabled:Bool = true
+    static var IsSecurionPayEnabled:Bool = true
+    static var IsAuthorizeEnabled:Bool = true
+    static var upgradeProAmount:Double = 9.0
+    
+   
+    
+}
+extension UIColor {
+    @nonobjc class var mainColor: UIColor {
+//        return UIColor.hexStringToUIColor(hex: "#FF8216")
+        return UIColor.hexStringToUIColor(hex: "#FF0000")
+    }
+    @nonobjc class var ButtonColor: UIColor {
+       // return UIColor.hexStringToUIColor(hex: "#FF8216")
+        return UIColor.hexStringToUIColor(hex: "#FF0000")    
+    }
+    @nonobjc class var lightButtonColor: UIColor {
+       // return UIColor.hexStringToUIColor(hex: "#FF8216")
+        return UIColor.ButtonColor.withAlphaComponent(0.25)
+    }
+    @nonobjc class var lightMainColor: UIColor {
+//        return UIColor.hexStringToUIColor(hex: "#FF8216")
+        return UIColor.mainColor.withAlphaComponent(0.25)
+    }
+}
